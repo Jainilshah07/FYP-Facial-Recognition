8f2b3bf0fa232bea4c2ee1cd734b2fbefb3a6089
JainilShah07 <shahjainilb@gmail.com>
2024-04-23 20:30:52 +0530

    improved Video component

--- frontend/src/pages/video/VideoProcessing.jsx
+++ frontend/src/pages/video/VideoProcessing.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { imgDb } from '../../firebase'; // Updated import for Firebase Storage
+import { imgDb, txtDb } from "../../firebase";
+import { doc, setDoc } from "firebase/firestore";
 import { listAll, getDownloadURL, ref, uploadBytes } from 'firebase/storage'; // Updated Firebase Storage imports
 
 const VideoProcessing = () => {
     const [videos, setVideos] = useState([]);
+    const [videoDetails, setVideoDetails] = useState({
+        Name: ""
+    });
     const [newVideo, setNewVideo] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
     useEffect(() => {
         fetchVideosFromDatabase();
@@ -22,33 +27,58 @@ const VideoProcessing = () => {
         }
     };
 
-    const handleUpload = (e) => {
-        const file = e.target.files[0];
-        // const storageRef = ref(imgDb, `videos/${file.name}`); // Update reference to use imgDb
-        // console.log(e.target.files[0]);
-        const videos = ref(imgDb, `Videos/${file.name}`)
-        uploadBytes(videos, e.target.files[0]).then(data => {
-            console.log(data, "videos")
-            getDownloadURL(data.ref).then(val => {
-                setNewVideo(val)
-            })
-        })
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
     }
 
     const handleClick = async () => {
-        alert("Video added successfully");
-        // navigate('/employee-details');
-        setNewVideo('');
+        if (!selectedFile) {
+            alert("Please select a video file to upload.");
+            return;
+        }
+
+        const videos = ref(imgDb, `Videos/${videoDetails.Name}`);
+        uploadBytes(videos, selectedFile).then(data => {
+            getDownloadURL(data.ref).then(val => {
+                setNewVideo(val);
+                const docName = `Vid_${videoDetails.Name.replace(/\s+/g, '_')}`;
+                setDoc(doc(txtDb, "Videos", docName), {
+                    Name: videoDetails.Name,
+                    vidUrl: val
+                });
+                alert("Video added successfully");
+                setSelectedFile(null); // Reset the selectedFile state
+                setVideoDetails({ Name: "" })
+            });
+        }).catch(error => {
+            console.error('Error uploading video:', error);
+            alert('Error uploading video. Please try again.');
+        });
     }
+    
 
     return (
         <div>
             <h1 className="text-3xl font-bold my-4">Video Processing</h1>
             <div className="mb-3">
+            </div>
+            <div className='col-span-2 my-3'>
+                <label htmlFor="inputName" className="col-span-1 text-left text-lg font-semibold mx-2 my-2">
+                    Name:
+                </label>
+                <input
+                    type="text"
+                    className="col-span-1 form-input px-2  rounded"
+                    id="inputName"
+                    placeholder="Enter Name"
+                    onChange={(e) =>
+                        setVideoDetails({ ...videoDetails, Name: e.target.value })
+                    }
+                />
                 <label htmlFor="inputFile" className="text-lg font-semibold mx-2 my-2">
                     Select Video:
                 </label>
-                <input type="file" onChange={(e) => handleUpload(e)} />
+                <input type="file" onChange={handleFileChange}  />
                 <button onClick={handleClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Upload Video</button>
             </div>
             <div>
